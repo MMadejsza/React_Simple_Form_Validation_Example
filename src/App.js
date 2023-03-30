59024df3c3c4cd2ae8a0afd872bd11ee2fe600c1
Maciej Madejsza <maciej.madejsza@gmail.com>
2023-03-30 12:52:52 +0100

    Initial Form 2/3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
 import React from 'react';
 
 class App extends React.Component {
-	state = {};
+	state = {
+		userName: '',
+	};
+
+	handleChange = (e) => {
+		console.log(e.target.type);
+		console.log(e.target.name);
+		const value = e.target.value;
+		this.setState({
+			userName: value,
+			userEmail: '',
+		});
+	};
 
 	render() {
-		return <div className='App'>Form</div>;
+		return (
+			<div className='App'>
+				<form action=''>
+					<label htmlFor='user'>
+						Your Name:
+						<input
+							type='text'
+							id='user'
+							name='userName'
+							value={this.state.userName}
+							onChange={this.handleChange}
+						/>
+					</label>
+					<label htmlFor='user'>
+						Your Email:
+						<input
+							type='email'
+							id='email'
+							name='userEmail'
+							value={this.state.userEmail}
+							onChange={this.handleChange}
+						/>
+					</label>
+					<label htmlFor='user'>
+						Your Name:
+						<input
+							type='text'
+							id='user'
+							name='userName'
+							value={this.state.userName}
+							onChange={this.handleChange}
+						/>
+					</label>
+				</form>
+			</div>
+		);
 	}
 }
 
